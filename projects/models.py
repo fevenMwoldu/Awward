34c388b32e6715c2527326eed92038e262072ffd
feven <feven.m.woldu@gmail.com>
2019-08-01 13:26:37 +0300

    created my models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    Title = models.CharField(max_length =30)
+    Landingpage_img = models.ImageField(upload_to = 'photos/')
+    Description = models.CharField(max_length =300)
+    Livelink = models.CharField(max_length =100)
+   
+    
+
+class Profile(models.Model):
+    Profile_pic = models.ImageField(upload_to = 'photos/')
+    User_bio = models.CharField(max_length =300)
+    Project = models.ForeignKey(Project)
+    Address = models.CharField(max_length =100)
