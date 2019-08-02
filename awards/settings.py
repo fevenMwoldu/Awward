81c0f0834b4d7b77d1f7e544ebcc4ad70b38d648
feven <feven.m.woldu@gmail.com>
2019-08-02 12:22:02 +0300

    created a static folder in the Root folder with a .css file inside it and added it's path on the base.html.For this to work i added the statis url and staticfiles_dirs in the settings.py file

--- awards/settings.py
+++ awards/settings.py
@@ -127,3 +127,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
