a0fa17ffc095a142ed2fc0bfac94c3af36c3b5c0
feven <feven.m.woldu@gmail.com>
2019-08-03 08:31:19 +0300

    Implemented Adding of a project for user through the add_project definition

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,HttpResponseRedirect
 from . models import Project,Profile
 from django.contrib.auth.decorators import login_required
+from .forms import photoForm, ProjectForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
-    projects = Project.objects.all()
-    return render(request, 'index.html',{"projects" : projects})
+        current_user = request.user    
+
+        if not Profile.user_has_profile(current_user.id):
+                return HttpResponseRedirect('profile')
+        projects = Project.objects.all()
+        return render(request, 'index.html',{"projects" : projects})
+
+
+@login_required(login_url='/account/login/')
+def add_project(request):
+    print('add_project is called ....')
+    current_user = request.user
+    form = ProjectForm()
+    
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.profile = Profile.objects.filter(user_id = current_user.id).first()
+            project.save()
+            print('Saved project {}'.format(project))
+    
+            return HttpResponseRedirect('/')
+        
+    return render(request, 'add_project.html', {"form": form})
